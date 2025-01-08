00e8dfca4c4f51d5b5c7f6b3bdb8ab3e397bb2d5
AriannaCrisriani <ariannacristiani@yahoo.it>
2025-01-08 17:10:44 +0100

    collegamento rotta show con il database e verifica di entrambe le rotte su postman

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,9 +1,10 @@
 const connection = require('../data/db.js');
 
+
+
+//ROTTA INDEX:
+
 function index(req, res) {
-    // res.json({
-    //     message: 'moviesIndex'
-    // })
 
     const sql = `SELECT * FROM movies`;
 
@@ -12,14 +13,31 @@ function index(req, res) {
 
         res.json(movies)
     })
-}
+};
 
+
+
+//ROTTA SHOW:
 
 function show(req, res) {
-    res.json({
-        message: 'moviesShow'
+
+    const id = parseInt(req.params.id);
+
+    const sql = `SELECT * FROM movies WHERE id=?`;
+
+    connection.query(sql, [id], (err, result) => {
+        if (err) return res.status(500).json({ message: err.message });
+
+        if (result.length === 0) return res.status(404).json({
+            error: 'not found',
+            message: 'movie not found'
+        });
+
+        const movie = result[0];
+
+        res.json(movie)
     })
-}
+};
 
 
 module.exports = { index, show }
