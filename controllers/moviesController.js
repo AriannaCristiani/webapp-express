befa97c8be0224096a14616e02d1f9700efdf1e8
AriannaCrisriani <ariannacristiani@yahoo.it>
2025-01-08 16:38:50 +0100

    test su postman delle rotte per verificare che siano state correttamente collegate

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,12 +1,16 @@
 const connection = require('../data/db.js');
 
 function index(req, res) {
-
+    res.json({
+        message: 'moviesIndex'
+    })
 }
 
 
 function show(req, res) {
-
+    res.json({
+        message: 'moviesShow'
+    })
 }
 
 
