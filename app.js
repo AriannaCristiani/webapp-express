2dbd006190a391686f7d79a88fa5a278f36a6c68
AriannaCrisriani <ariannacristiani@yahoo.it>
2025-01-08 16:53:01 +0100

    collegamento rotta index con il database

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 
-app.use(express.static('public/images'))
+app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,8 +1,16 @@
 const connection = require('../data/db.js');
 
 function index(req, res) {
-    res.json({
-        message: 'moviesIndex'
+    // res.json({
+    //     message: 'moviesIndex'
+    // })
+
+    const sql = `SELECT * FROM movies`;
+
+    connection.query(sql, (err, movies) => {
+        if (err) return res.status(500).json({ message: err.message })
+
+        res.json(movies)
     })
 }
 
