f72ae4c86e65ac5c86a757c878495a6800cae996
AriannaCrisriani <ariannacristiani@yahoo.it>
2025-01-08 15:43:21 +0100

    importazione foto delle cover su public e test della rotta index con postman

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const app = express();
 const port = 3000;
 
 
+app.use(express.static('public/images'))
 
 
-
+app.get('/', (req, res) => {
+    console.log('il server sta funzionando')
+})
 
 
 app.listen(port, () => {
