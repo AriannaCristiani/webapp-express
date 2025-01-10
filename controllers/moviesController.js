8f4b9c98357740bda024516dd687c195722d0b96
AriannaCrisriani <ariannacristiani@yahoo.it>
2025-01-10 12:43:09 +0100

    modifiche rotta show

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -27,7 +27,13 @@ function show(req, res) {
 
     const id = parseInt(req.params.id);
 
-    const sql = `SELECT * FROM movies WHERE id=?`;
+    const sql = `SELECT movies.*, AVG(vote) AS avg_vote 
+		FROM movies
+		JOIN reviews
+		ON movies.id = reviews.movie_id 
+		WHERE movies.id = ?
+		GROUP BY movies.id
+		`
 
     connection.query(sql, [id], (err, result) => {
         if (err) return res.status(500).json({ message: err.message });
@@ -38,10 +44,19 @@ function show(req, res) {
         });
 
         const movie = result[0];
+        movie.image = `http://localhost:3000/img/${movie.image}`
 
-        res.json(movie)
+        const sql = `SELECT * FROM reviews WHERE movie_id = ?`
+
+        connection.query(sql, [id], (err, results) => {
+            if (err) return res.status(500).json({ message: err.message })
+
+            movie.reviews = results
+
+            res.json(movie)
+        })
     })
-};
+}
 
 
 module.exports = { index, show }
